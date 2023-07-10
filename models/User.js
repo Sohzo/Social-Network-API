11073d96f395308b5e2549792d9d6e8ffa0de8ef
Jonathan Winter <jonpw2004@outlook.com>
2023-07-10 16:26:23 -0600

    Imported template and setup user schema

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require('mongoose');
+var validator = require('validator');
+const { default: isEmail } = require('validator/lib/isEmail');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: [ isEmail, 'Invalid email']
+    },
+    
+    thoughts: {
+      type: Schema.Types.ObjectId,
+      ref: 'thought'
+    },
+
+    friends: {
+      type: Schema.Types.ObjectId,
+      ref: 'user'
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+
+    id: false,
+  }
+);
+
+const User = model('User', userSchema)
+userSchema.virtual('friendCount').get(function() {
+  return this.friends.length;
+});
+
+module.exports = User;
