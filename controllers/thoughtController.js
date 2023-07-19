3ba436d135c5c5a57646b1ef7fed97b78ea5e65c
Jonathan Winter <jonpw2004@outlook.com>
2023-07-18 19:01:57 -0600

    Finished user routes/controller

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -3,14 +3,19 @@ const { Thought, User } = require('../models');
 const thoughtController = {
 
     getAllThoughts(req,res) {
-        Thought.find({}).populate({
-            path: 'user',
+        Thought.find({})
+        .populate({
+            path: 'reactions',
             select: '-__v'
+        })
+        .populate({
+            path: "thoughts",
+            select: "__v"
         })
         .select('-__v')
         .sort({ _id: -1 })
         .then(dbThoughtData => res.json(dbThoughtData))
-        .catch(err => {
+        .catch((err) => {
             console.log(err);
             res.status(500).json(err)
         });
